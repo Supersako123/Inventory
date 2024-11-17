f73d8a2d31ce2d0d43e90bba59a012b36bd50829
Jonathan <infiniteSako@gmail.com>
2024-11-16 21:04:05 -0800

    changed tailwind configuration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,6 @@
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
-        <script src="https://cdn.tailwindcss.com"></script>
         <!-- Styles -->
         <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     </head>
